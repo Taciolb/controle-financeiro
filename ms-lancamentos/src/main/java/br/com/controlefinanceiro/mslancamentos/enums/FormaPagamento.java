7432074d4cdd263eccc2b9d4379523bc9f348033
Taciano <taciolb@gmail.com>
2026-06-17 19:54:05 -0300

    feat: add enum FormaPagamento

---
+++ ms-lancamentos/src/main/java/br/com/controlefinanceiro/mslancamentos/enums/FormaPagamento.java
@@ -0,0 +1,10 @@
+package br.com.controlefinanceiro.mslancamentos.enums;
+
+public enum FormaPagamento {
+    DINHEIRO,
+    PIX,
+    BOLETO,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    TRANSFERENCIA
+}
