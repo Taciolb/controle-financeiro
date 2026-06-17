e89b4a1401577169d6d2f6e98ae326eaf0330015
Taciano <taciolb@gmail.com>
2026-06-16 21:28:08 -0300

    feat: add colunas valorOriginal, taxaJuros, tipoJuros, juros e desconto

--- ms-lancamentos/src/main/resources/db/migration/V4__add_valor_original_juros_desconto.sql
+++ ms-lancamentos/src/main/resources/db/migration/V4__add_valor_original_juros_desconto.sql
@@ -1,10 +1,13 @@
 ALTER TABLE lancamentos
     ADD COLUMN valor_original NUMERIC(15, 2),
+    ADD COLUMN taxa_juros     NUMERIC(5, 2)  DEFAULT 0,
+    ADD COLUMN tipo_juros     VARCHAR(10),
     ADD COLUMN juros          NUMERIC(15, 2) DEFAULT 0,
     ADD COLUMN desconto       NUMERIC(15, 2) DEFAULT 0;
 
 UPDATE lancamentos
 SET valor_original = valor,
+    taxa_juros     = 0,
     juros          = 0,
     desconto       = 0;
 
